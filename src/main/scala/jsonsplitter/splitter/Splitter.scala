1a569011b28a137db0e1b10c263e13dc3a02fdb5
Jean-Luc CANELA <jlcanelam@gmail.com>
2021-03-02 22:38:51 +0100

    add gunzip

--- src/main/scala/jsonsplitter/splitter/Splitter.scala
+++ src/main/scala/jsonsplitter/splitter/Splitter.scala
@@ -34,23 +34,30 @@ object Splitter {
         feedQueue = queue.offerAll("a sample data".getBytes.toIterable)
         stream    = ZStream.fromQueueWithShutdown(queue)
       } yield (feedQueue, stream)
-      
-      def writeS3Data(zs: ZStream[Blocking with S3 with Console, Throwable, Byte], bucket: String, path: String) = for {
-         _<- console.putStrLn(s"write to s3://$bucket/$path")
+
+      def writeS3Data(
+          zs: ZStream[Blocking with S3 with Console, Throwable, Byte],
+          bucket: String,
+          path: String
+      ) = for {
+        _ <- console.putStrLn(s"write to s3://$bucket/$path")
         _ <- multipartUpload(bucket, path, zs)(4)
       } yield ()
-       
+
       def writeFileData(zs: ZStream[Any, Throwable, Byte], path: Path) = for {
-        _ <- ZIO.fromOption(Option(path.getParent)).map(path => Files.createDirectories(path)) orElse ZIO.succeed(())
+        _ <- ZIO
+          .fromOption(Option(path.getParent))
+          .map(path => Files.createDirectories(path)) orElse ZIO.succeed(())
         _ <- zs.run(ZSink.fromFile(path))
       } yield ()
-        
+
       def createQueue(destination: JPath, name: String) = for {
         queue <- Queue.unbounded[String]
         zs = ZStream.fromQueueWithShutdown(queue).mapConcatChunk(s => Chunk.fromArray(s.getBytes()))
         _ <- destination match {
-          case LocalFile(path: Path)  => writeFileData(zs, path.resolve(name)).fork
-          case S3File(bucket: String, path: String) => writeS3Data(zs, bucket, s"${path}/${name}").fork
+          case LocalFile(path: Path) => writeFileData(zs, path.resolve(name)).fork
+          case S3File(bucket: String, path: String) =>
+            writeS3Data(zs, bucket, s"${path}/${name}").fork
         }
       } yield queue
 
@@ -58,27 +65,35 @@ object Splitter {
         _ <- console.putStrLn("showing buckets")
         st = createStream(S3File("mybucket", "in/sample.json"))
         queue <- createQueue(S3File("mybucket", "out"), "default")
-        _ <- st.run(Sink.foreach((s: String) => queue.offer(s))).ensuring(queue.shutdown)
-        _ <- ZIO.succeed(()).delay(Duration.ofMillis(1500))
+        _     <- st.run(Sink.foreach((s: String) => queue.offer(s))).ensuring(queue.shutdown)
+        _     <- ZIO.succeed(()).delay(Duration.ofMillis(1500))
       } yield ()
 
-      def createS3Stream(path: S3File): ZStream[S3, Throwable, String] = {
+      def createS3Stream(path: S3File): ZStream[S3, Throwable, Byte] =
         s3.getObject(path.bucket, path.path)
-          .transduce(ZTransducer.utfDecode)
-          .transduce(ZTransducer.splitLines)
+
+      def createLocalStream(path: Path): ZStream[Blocking, Throwable, Byte] =
+        ZStream.fromFile(path)
+
+      def gunzip(stream: ZStream[S3 with Blocking, Throwable, Byte], path: String) = if (
+        path.endsWith(".gz")
+      ) {
+        stream.transduce(ZTransducer.gunzip(1024 * 64))
+      } else {
+        stream
       }
 
-      def createLocalStream(path: Path): ZStream[Blocking, Throwable, String] =
-        ZStream
-          .fromFile(path)
+      def createStream(path: JPath): ZStream[S3 with Blocking, Throwable, String] = {
+
+        def stream = path match {
+          case path @ S3File(_, key) => gunzip(createS3Stream(path), key)
+          case LocalFile(path)       => gunzip(createLocalStream(path), path.toString)
+        }
+
+        stream
           .transduce(ZTransducer.utf8Decode)
           .transduce(ZTransducer.splitLines)
-
-      def createStream(path: JPath): ZStream[S3 with Blocking, Throwable, String] =
-        path match {
-          case path: S3File    => createS3Stream(path)
-          case LocalFile(path) => createLocalStream(path)
-        }
+      }
 
       val ExtractName = """.*\"nodeType\"\s*\:\s*"([a-zA-Z0-9_-]+)\".*""".r
       def findName(line: String) = ExtractName
@@ -89,7 +104,11 @@ object Splitter {
       def addQueue(destination: JPath)(
           state: Map[String, Queue[String]],
           name: String
-      ): ZIO[Blocking with S3 with Console, Throwable, (Map[String, Queue[String]], Queue[String])] = for {
+      ): ZIO[
+        Blocking with S3 with Console,
+        Throwable,
+        (Map[String, Queue[String]], Queue[String])
+      ] = for {
         queue <- createQueue(destination, name)
       } yield (state + (name -> queue), queue)
 
@@ -113,8 +132,8 @@ object Splitter {
           stream <- IO.succeed(createStream(path))
           init = Map[String, Queue[String]]()
           finalState <- stream.foldM(init)(write(destination))
-          _ <- ZIO.foreachPar(finalState.values)(_.shutdown)
-          _ <- ZIO.succeed(()).delay(Duration.ofMillis(1500))
+          _          <- ZIO.foreachPar(finalState.values)(_.shutdown)
+          _          <- ZIO.succeed(()).delay(Duration.ofMillis(3000))
         } yield ()
       }
     }
--- src/test/scala/jsonsplitter/acceptance/CommandSpec.scala
+++ src/test/scala/jsonsplitter/acceptance/CommandSpec.scala
@@ -16,18 +16,18 @@ object CommandSpec extends DefaultRunnableSpec {
   val params                                             = Array("split-json", "src/test/resources/sample.json", "out-test")
   def run(args: Array[String]): ZIO[Any, Throwable, Int] = SplitJsonCli.cmd(args).provideLayer(deps)
 
- /* def runAndRead = for {
+  /* def runAndRead = for {
     _       <- run(params)
     //_  <- ZIO.effect(Thread.sleep(500))
     //_ <- ZIO.succeed(()).delay(Duration.ofMillis(500))
     //content <- IO(scala.io.Source.fromFile("out-test/a.json").getLines.mkString)
    // _       <- console.putStrLn(content)
   } yield content
-*/
+   */
   def spec = suite("Command")(
     testM("split json must succeed") {
       assertM(run(params))(equalTo(SUCCESS))
-    }/*,
+    } /*,
     testM("split json must create a file") {
       assertM(runAndRead)(equalTo("""{ "nodeType": "a"}"""))
     }*/
